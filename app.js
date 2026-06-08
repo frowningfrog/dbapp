8ae754867908290c1b12bb01cfee835e8c7dcb4f
frowningfrog <natalizedstolpinator@hotmail.com>
2026-06-08 09:28:16 -0600

    uri

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const MONGO_URI = "mongodb://localhost:27017/mtec";
+// const MONGO_URI = "mongodb://localhost:27017/mtec";
+const MONGO_URI = "mongodb://127.0.0.1:27017";
 
 mongoose.connect(MONGO_URI);
 const db = mongoose.connection;
