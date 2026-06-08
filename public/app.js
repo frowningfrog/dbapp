c59f2bab760af6ccfddc4967976581ec31320994
frowningfrog <natalizedstolpinator@hotmail.com>
2026-06-08 09:57:08 -0600

    updates

--- public/app.js
+++ public/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const MONGO_URI = "mongodb://localhost:27017/mtec";
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/mtec";
 
 mongoose.connect(MONGO_URI);
 const db = mongoose.connection;
